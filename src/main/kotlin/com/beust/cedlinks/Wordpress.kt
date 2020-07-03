0abde5197e55327f57223ea4b9f6b763142c3281
Cedric Beust <4290041+ionisaligox72@users.noreply.github.com>
2020-07-03 13:32:01 -0700

    Wordpress API

---
+++ src/main/kotlin/com/beust/cedlinks/Wordpress.kt
@@ -0,0 +1,67 @@
+package com.beust.cedlinks
+
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import java.io.IOException
+
+
+
+
+class Wordpress {
+    class BasicAuthInterceptor(user: String?, password: String?) : Interceptor {
+        private val credentials: String
+
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val request: Request = chain.request()
+            val authenticatedRequest: Request = request.newBuilder()
+                    .header("Authorization", credentials).build()
+            return chain.proceed(authenticatedRequest)
+        }
+
+        init {
+            credentials = Credentials.basic(user, password)
+        }
+    }
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    private val basicAuthInterceptor = BasicAuthInterceptor("", "")
+    private val gson = GsonBuilder().setLenient().create()
+    private val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .addInterceptor(basicAuthInterceptor)
+            .build()
+    private val retrofit = Retrofit.Builder()
+            .baseUrl("http://beust.com/weblog/wp-json/wp/v2/")
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(WordpressService::class.java)
+
+    interface WordpressService {
+        @Headers("Content-Type: application/json; charset=UTF-8", "X-Accept: application/json")
+        @POST("posts")
+        fun post(@Body request: PostRequest): Call<PostResponse>
+    }
+
+    class PostRequest(val title: String, val content: String, val excerpt: String)
+    class PostResponse(val id: Long, val date: String)
+
+    fun post(title: String, content: String, excerpt: String) {
+        val r = retrofit.post(PostRequest(title, content, excerpt)).execute()
+        println("ID: " + r.body()?.id)
+    }
+}
+
+fun main() {
+    Wordpress().post("from Kotlin", "content Kotlin", "excerpt Kotlin")
+}
