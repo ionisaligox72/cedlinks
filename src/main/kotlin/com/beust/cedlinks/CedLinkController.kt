a88be096d59611d9f9c5506052ca76097df0310e
Cedric Beust <4290041+ionisaligox72@users.noreply.github.com>
2020-07-19 16:05:26 -0700

    More endpoints

--- src/main/kotlin/com/beust/cedlinks/CedLinkController.kt
+++ src/main/kotlin/com/beust/cedlinks/CedLinkController.kt
@@ -1,34 +1,64 @@
 package com.beust.cedlinks
 
-import io.micronaut.context.ApplicationContext
-import io.micronaut.context.annotation.Value
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.MediaType
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.*
 import io.micronaut.http.annotation.Produces
-import io.micronaut.http.annotation.QueryValue
-import javax.annotation.PostConstruct
+import org.slf4j.LoggerFactory
+import java.net.URI
 import javax.inject.Inject
-import javax.inject.Named
-import javax.inject.Singleton
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.QueryParam
 
 @Controller("/")
 class CedLinkController @Inject constructor(private val dao: Dao) {
+    private val log = LoggerFactory.getLogger(CedLinkController::class.java)
 
     @Get("list")
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces
     fun list(@QueryValue("all", defaultValue = "false") all: Boolean) = dao.listLinks(all)
 
+    @Get("submit")
+    @Produces(MediaType.TEXT_HTML)
+    fun submitLink(
+            @QueryValue("url") url: String = "",
+            @QueryValue("title") title: String? = null,
+            @QueryValue("comment") comment: String? = null,
+            @QueryValue("time") time: String? = null
+    ): String {
+        val result = Template.render("submitLink.mustache", mapOf(
+                "url" to url,
+                "comment" to comment,
+                "title" to title,
+                "host" to Config.host,
+                "time" to time
+        ))
+        return result
+    }
 
-//    @Get("/")
-//    @Produces(MediaType.TEXT_PLAIN)
-//    fun index(@QueryValue("name", defaultValue = "Unknown") name: String): String {
-//        val context = ApplicationContext.run()
-//        val environment = context.environment
-//        val port = environment.getProperty("database.port", String::class.java)
-//        return "Hello $name: " + db.port
-//    }
+    @Post("insertLink")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    fun insertLink(url: String, title: String,
+            comment: String, imageUrl: String? = null,
+            time: String): HttpResponse<String> {
+        val result = try {
+            if (time != Config.time) {
+                HttpResponse.status<String>(HttpStatus.UNAUTHORIZED)
+            } else {
+                dao.insertLink(url, title, comment, imageUrl)
+                HttpResponse.redirect<String>(URI(url))
+            }
+        } catch(ex: Exception) {
+            log.error("Error: " + ex.message, ex)
+            HttpResponse.serverError<String>().body(ex.message)
+        }
+        return result
+    }
+
+    @Get("preview")
+    @Produces(MediaType.TEXT_HTML)
+    fun publish(): String = dao.preview()
 }
